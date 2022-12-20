fcfc66ef8146e75cfb600fdcdff732fb97aef10b
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2022-12-21 09:53:33 +1100

    Add top players image and rework layering for outlines and blurs

--- gen_wrap.py
+++ gen_wrap.py
@@ -40,18 +40,28 @@ def mechanics_image(mechanics, year, output_path):
     max_lens = [14, 16, 14, 19, 14]
     for mechanic, pos, max_len in zip(mechanics_plays[:min(5, len(mechanics_plays))], positions, max_lens):
         text = textwrap.fill(mechanic, max_len)
+        lines = text.splitlines()
         height = get_text_height(text, font)
         pos = (pos[0], pos[1] - height)
+        pos_store = pos
 
-        for line in text.splitlines():
+        blurred = Image.new('RGBA', img.size)
+        draw_b = ImageDraw.Draw(blurred)
+
+        for line in lines:
             # blurred backdrop
-            blurred = Image.new('RGBA', img.size)
-            draw_b = ImageDraw.Draw(blurred)
             draw_b.text(pos, line, blur_colour, font, stroke_width=5)
-            blurred = blurred.filter(ImageFilter.BoxBlur(5))
 
-            img.paste(blurred, blurred)
+            # update pos for next line
+            height = get_text_height(line, font)
+            pos = (pos[0], pos[1] + height)
 
+        # blur outlined text
+        blurred = blurred.filter(ImageFilter.BoxBlur(5))
+        img.paste(blurred, blurred)
+
+        pos = pos_store
+        for line in lines:
             # regular text
             draw.text(pos, line, text_colour, font)
 
@@ -64,7 +74,7 @@ def mechanics_image(mechanics, year, output_path):
 
 def games_image(games, year, output_path):
     games_plays = [game for game in games.values() if game.plays[year]]
-    games_plays.sort(key=lambda game: game.plays[year], reverse=True)   
+    games_plays.sort(key=lambda game: game.plays[year], reverse=True)
 
     text_colour = (251, 194, 0)
     outline_colour_0 = (0, 0, 0)
@@ -74,28 +84,75 @@ def games_image(games, year, output_path):
     img = Image.open('images/games.png')
     draw = ImageDraw.Draw(img)
 
-    positions = [(356, 500), (1212, 766), (356, 1040), (1212, 1297), (356, 1545)]
+    positions = [(356, 500), (1212, 766), (356, 1040),
+                 (1212, 1297), (356, 1545)]
     max_lens = [14, 14, 14, 14, 14]
     anchors = ['lt', 'rt', 'lt', 'rt', 'lt']
     for game, pos, max_len, anchor in zip(games_plays[:min(5, len(games_plays))], positions, max_lens, anchors):
-        
         text = textwrap.fill(game.name, max_len)
+        lines = text.splitlines()
         height = get_text_height(text, font)
         pos = (pos[0], pos[1] - height)
+        pos_store = pos
 
-        for line in text.splitlines():
+        for line in lines:
             # outline
-            draw.text(pos, line, outline_colour_1, font, anchor=anchor, stroke_width=10)
-            draw.text(pos, line, outline_colour_0, font, anchor=anchor, stroke_width=5)
+            draw.text(pos, line, outline_colour_1, font,
+                      anchor=anchor, stroke_width=10)
 
+            # update pos for next line
+            height = get_text_height(line, font)
+            pos = (pos[0], pos[1] + height)
+
+        pos = pos_store
+        for line in lines:
+            draw.text(pos, line, outline_colour_0, font,
+                      anchor=anchor, stroke_width=5)
+
+            # update pos for next line
+            height = get_text_height(line, font)
+            pos = (pos[0], pos[1] + height)
+
+        pos = pos_store
+        for line in lines:
             # regular text
             draw.text(pos, line, text_colour, font, anchor=anchor)
 
             # update pos for next line
             height = get_text_height(line, font)
             pos = (pos[0], pos[1] + height)
-            
+
     img.save(os.path.join(output_path, 'games.png'))
+
+
+def players_image(players, year, output_path):
+    players_plays = [player for player in players.values()
+                     if player.plays[year]]
+    players_plays.sort(key=lambda player: player.plays[year], reverse=True)
+
+    text_colour = (0, 0, 0)
+    font = ImageFont.truetype('fonts/IBMPlexSans-Thin.ttf', 64)
+
+    img = Image.open('images/players.png')
+    draw = ImageDraw.Draw(img)
+
+    positions = [(220, 380), (220, 500), (220, 620),
+                 (220, 740), (220, 860)]
+    max_lens = [15, 15, 15, 15, 15]
+    for player, pos, max_len in zip(players_plays[:min(5, len(players_plays))], positions, max_lens):
+        text = textwrap.fill(player.name.upper(), max_len)
+        height = get_text_height(text, font)
+        pos = (pos[0], pos[1] - height)
+
+        for line in text.splitlines():
+            # regular text
+            draw.text(pos, line, text_colour, font, anchor='lt')
+
+            # update pos for next line
+            height = get_text_height(line, font)
+            pos = (pos[0], pos[1] + height)
+
+    img.save(os.path.join(output_path, 'players.png'))
 
 
 def main():
@@ -139,6 +196,8 @@ def main():
 
     mechanics_image(mechanics, args.year, args.output)
     games_image(games, args.year, args.output)
+    players_image(players, args.year, args.output)
+
 
 if __name__ == '__main__':
     main()
