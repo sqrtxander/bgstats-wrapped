2ff4e040d8c9e8d6150b3cc3b447de5075a81530
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2022-12-26 16:59:56 +1100

    add bar charts to visuals

--- gen_wrap.py
+++ gen_wrap.py
@@ -10,6 +10,8 @@ import argparse
 
 from collections import defaultdict
 
+import matplotlib.pyplot as plt
+import numpy
 
 from get_data import *
 
@@ -46,7 +48,7 @@ def plays_image(
 
     text_colour = (0, 0, 0)
     font = ImageFont.truetype('fonts/verdanab.ttf', 72)
-    
+
     img = Image.open('images/plays.png')
     draw = ImageDraw.Draw(img)
 
@@ -54,21 +56,21 @@ def plays_image(
     draw.text((1584, 1557), unique_games, text_colour, font, anchor='mm')
 
     img.save(os.path.join(output_path, '01plays.png'))
-    
+
 
 def games_image(
     games: dict[int | None, Game],
     year: int | None,
     output_path: str
 ) -> None:
-    games_plays = [game for game in games.values() if game.plays[year]]
-    games_plays.sort(key=lambda game: game.plays[year], reverse=True)
+    top_games = [game for game in games.values() if game.plays[year]]
+    top_games.sort(key=lambda game: game.plays[year], reverse=True)
+    top_games = top_games[:min(5, len(top_games))]
 
     text_colour = (251, 194, 0)
     outline_colour_0 = (255, 255, 255)
     outline_colour_1 = (0, 0, 0)
-    font = ImageFont.truetype('fonts/SATANICK.TTF', 96)
-
+    font = ImageFont.truetype('fonts/SATANICK.TTF', 84)
     img = Image.open('images/games.png')
 
     img_outline_0 = Image.new('RGBA', img.size)
@@ -80,16 +82,17 @@ def games_image(
     img_text = Image.new('RGBA', img.size)
     draw_text = ImageDraw.Draw(img_text)
 
-    positions = [(356, 500), (1212, 766), (356, 1040),
-                 (1212, 1297), (356, 1545)]
+    positions = [(356, 482), (1212, 732), (356, 982),
+                 (1212, 1233), (356, 1483)]
     max_lens = [14, 14, 14, 14, 14]
     anchors = ['lt', 'rt', 'lt', 'rt', 'lt']
-    for game, pos, max_len, anchor in zip(games_plays[:min(5, len(games_plays))], positions, max_lens, anchors):
+    scale = (img.width - 30) / top_games[0].plays[year]
+
+    for game, pos, max_len, anchor in zip(top_games, positions, max_lens, anchors):
         text = textwrap.fill(game.name, max_len)
         lines = text.splitlines()
         height = get_text_height(text, font)
         pos = (pos[0], pos[1] - height)
-        pos_store = pos
 
         for line in lines:
             # outline 0
@@ -107,6 +110,14 @@ def games_image(
             height = get_text_height(line, font)
             pos = (pos[0], pos[1] + height)
 
+        # add bar chart
+        draw_outline_0.rectangle(
+            (15, pos[1]+10, 15 + game.plays[year] * scale, pos[1] + 50), outline_colour_0)
+        draw_outline_1.rectangle(
+            (20, pos[1]+15, 10 + game.plays[year] * scale, pos[1] + 45), outline_colour_1)
+        draw_text.rectangle(
+            (25, pos[1]+20, 5 + game.plays[year] * scale, pos[1] + 40), text_colour)
+
     # add outline and text
     img.paste(img_outline_0, img_outline_0)
     img.paste(img_outline_1, img_outline_1)
@@ -120,9 +131,10 @@ def players_image(
     year: int | None,
     output_path: str
 ) -> None:
-    players_plays = [player for player in players.values()
-                     if player.plays[year]]
-    players_plays.sort(key=lambda player: player.plays[year], reverse=True)
+    top_players = [player for player in players.values()
+                   if player.plays[year]]
+    top_players.sort(key=lambda player: player.plays[year], reverse=True)
+    top_players = top_players[:min(5, len(top_players))]
 
     text_colour = (0, 0, 0)
     font = ImageFont.truetype('fonts/IBMPlexSans-Thin.ttf', 64)
@@ -132,10 +144,12 @@ def players_image(
     img_text = Image.new('RGBA', img.size)
     draw_text = ImageDraw.Draw(img_text)
 
-    positions = [(220, 380), (220, 500), (220, 620),
-                 (220, 740), (220, 860)]
+    positions = [(220, 360), (220, 480), (220, 600),
+                 (220, 720), (220, 840)]
     max_lens = [15, 15, 15, 15, 15]
-    for player, pos, max_len in zip(players_plays[:min(5, len(players_plays))], positions, max_lens):
+    scale = (img.width - 440) / top_players[0].plays[year]
+
+    for player, pos, max_len in zip(top_players, positions, max_lens):
         text = textwrap.fill(player.name.upper(), max_len)
         height = get_text_height(text, font)
         pos = (pos[0], pos[1] - height)
@@ -148,11 +162,16 @@ def players_image(
             height = get_text_height(line, font)
             pos = (pos[0], pos[1] + height)
 
+        # add bars
+        draw_text.rectangle(
+            (220, pos[1] - 5, 220 + player.plays[year] * scale, pos[1] + 15), text_colour)
+        draw_text.rectangle(
+            (222, pos[1] - 3, 218 + player.plays[year] * scale, pos[1] + 13), (0, 0, 0, 0))
+
     # add text
     img.paste(img_text, img_text)
 
     img.save(os.path.join(output_path, '03players.png'))
-
 
 
 def mechanics_image(
@@ -160,12 +179,12 @@ def mechanics_image(
     year: int | None,
     output_path: str
 ) -> None:
-    mechanics_plays = list(mechanics[year])
-    mechanics_plays.sort(
+    top_mechanics = list(mechanics[year])
+    top_mechanics.sort(
         key=lambda mechanic: mechanics[year][mechanic], reverse=True)
-
+    top_mechanics = top_mechanics[:min(5, len(top_mechanics))]
     text_colour = (237, 227, 217)
-    blur_colour = (0, 0, 0)
+    shadow_colour = (0, 0, 0)
     font = ImageFont.truetype('fonts/plump.ttf', 96)
 
     img = Image.open('images/mechanics.png')
@@ -177,8 +196,11 @@ def mechanics_image(
     draw_text = ImageDraw.Draw(img_text)
 
     positions = [(656, 174), (500, 491), (656, 803), (347, 1118), (656, 1428)]
+    bar_y_positions = [193, 339-30, 640, 1137, 1278-30]
     max_lens = [14, 16, 14, 19, 14]
-    for mechanic, pos, max_len in zip(mechanics_plays[:min(5, len(mechanics_plays))], positions, max_lens):
+    scale = (img.width - 72) / mechanics[year][top_mechanics[0]]
+
+    for mechanic, pos, bary, max_len in zip(top_mechanics, positions, bar_y_positions, max_lens):
         text = textwrap.fill(mechanic, max_len)
         lines = text.splitlines()
         height = get_text_height(text, font)
@@ -186,7 +208,7 @@ def mechanics_image(
 
         for line in lines:
             # img_shadow backdrop
-            draw_shadow.text(pos, line, blur_colour, font, stroke_width=5)
+            draw_shadow.text(pos, line, shadow_colour, font, stroke_width=5)
 
             # regular text
             draw_text.text(pos, line, text_colour, font)
@@ -194,6 +216,12 @@ def mechanics_image(
             # update pos for next line
             height = get_text_height(line, font)
             pos = (pos[0], pos[1] + height)
+
+        # add bars
+        draw_shadow.rectangle(
+            (31, bary, 21 + mechanics[year][mechanic] * scale, bary + 30), shadow_colour)
+        draw_text.rectangle(
+            (36, bary + 5, 16 + mechanics[year][mechanic] * scale, bary + 25), text_colour)
 
     # blur outlined text
     img_shadow = img_shadow.filter(ImageFilter.BoxBlur(5))
